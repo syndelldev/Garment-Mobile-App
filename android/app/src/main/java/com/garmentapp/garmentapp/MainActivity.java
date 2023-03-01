9e833e0b6852e019e00a8df3de5a8644c899e5f2
syndelldev <106510484+syndelldev@users.noreply.github.com>
2023-03-01 15:34:19 +0530

    code is upload

---
+++ android/app/src/main/java/com/garmentapp/garmentapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.garmentapp.garmentapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.garmentapp.garmentapp',
+  appName: 'garmentapp',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
